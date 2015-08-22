23bacea8b2dce57865861de6103b39874d4b5c1c
uselessd <boycottsystemd@openmailbox.org>
2015-08-21 21:33:44 -0400

    init_listing/2, trace/2, flush/1 and del_dir/1 helpers

--- src/eartc.erl
+++ src/eartc.erl
@@ -1,18 +1,36 @@
 -module('eartc').
 
 %% API exports
--export([]).
+-export([init_listing/2, trace/2, flush/1]).
 
+-define(PREFIX, "/usr/local/bin/").
+-define(EARTC_DIR, "/usr/share/eartc/").
 -define(MAKE, os:find_executable("make")).
--define(ARTRUN, "/usr/local/bin/artrun").
--define(ARTC, "/usr/local/bin/artc").
--define(STRACE_ARTC, "/usr/local/bin/strace-artc").
--define(GENINIT, "/usr/local/bin/geninit").
+
+-define(ARTRUN, string:join([?PREFIX, "artrun"], "")).
+-define(ARTC, string:join([?PREFIX, "artc"], "")).
+-define(STRACE_ARTC, string:join([?PREFIX, "strace-artc"], "")).
+-define(GENINIT, string:join([?PREFIX, "geninit"], "")).
 
 %%====================================================================
 %% API functions
 %%====================================================================
-trace(Program) ->
+
+init_listing(Program, Dirs) ->
+   ListingDir = file:make_dir(lists:concat([?EARTC_DIR, Program])),
+   Port = vrunfile(?GENINIT, Dirs, 
+                  lists:concat(
+                  [?EARTC_DIR, Program, "/", Program, ".init"])
+                  ),
+   ok.
+
+trace(Program, Args) ->
+   Port = cmdpp(?STRACE_ARTC ++ 
+                  " -o" ++
+                  lists:concat(
+                  [" ", ?EARTC_DIR, filename:basename(Program), "/", 
+                  filename:basename(Program), ".strace"]) ++
+                  " " ++ string:join([Program, Args], " ")),
    ok.
 
 compile(Trace) ->
@@ -20,17 +38,41 @@ compile(Trace) ->
 
 replay(Trace) ->
    ok.
-   
-init_listing(Dirs) ->
-   ok.
 
-flush(Trace) ->
+flush(Dir) ->
+   del_dir(filelib:wildcard(?EARTC_DIR ++ Dir)),
+   ok;
+flush(Dir) when Dir =:= everything -> %% {error, enoent}
+   del_dir(lists:reverse(filelib:wildcard(?EARTC_DIR ++ "*" ++ "*"))),
    ok.
 
 %%====================================================================
 %% Internal functions
 %% Some adapted from nerves-utils
 %%====================================================================
+
+del_dir(Dir) ->
+   lists:foreach(fun(D) ->
+                    ok = file:del_dir(D)
+                 end, del_all_files([Dir], [])).
+ 
+del_all_files([], EmptyDirs) ->
+   EmptyDirs;
+del_all_files([Dir | T], EmptyDirs) ->
+   {ok, FilesInDir} = file:list_dir(Dir),
+   {Files, Dirs} = lists:foldl(fun(F, {Fs, Ds}) ->
+                                  Path = Dir ++ "/" ++ F,
+                                  case filelib:is_dir(Path) of
+                                     true ->
+                                          {Fs, [Path | Ds]};
+                                     false ->
+                                          {[Path | Fs], Ds}
+                                  end
+                               end, {[],[]}, FilesInDir),
+   lists:foreach(fun(F) ->
+                         ok = file:delete(F)
+                 end, Files),
+   del_all_files(T ++ Dirs, [Dir | EmptyDirs]).
 
 run(Executable) ->
     run(Executable, [], []).
@@ -67,6 +109,16 @@ vrun(Executable, Args) ->
                   [exit_status, {args, Args}, stderr_to_stdout]),
          verbose_loop_till_done(Port)
     end.
+    
+vrunfile(Executable, Args, File) ->
+    case os:find_executable(Executable) of
+      false ->
+         exit(enoent);
+      FoundExecutable ->
+         Port = open_port({spawn_executable, FoundExecutable},
+                  [exit_status, {args, Args}, stderr_to_stdout]),
+         verbose_loop_till_done_file(File, Port)
+    end.
 
 loop_till_done(Port, Data) ->
     receive
@@ -76,6 +128,17 @@ loop_till_done(Port, Data) ->
          loop_till_done(Port, ConcatenatedData);
       {Port, {exit_status, 0}} ->
          {ok, Data};
+      {Port, {exit_status, ExitStatus}} ->
+         {error, ExitStatus}
+    end.
+    
+verbose_loop_till_done_file(File, Port) ->
+    receive
+      {Port, {data, NewData}} ->
+         file:write_file(File, NewData),
+         verbose_loop_till_done_file(File, Port);
+      {Port, {exit_status, 0}} ->
+         ok;
       {Port, {exit_status, ExitStatus}} ->
          {error, ExitStatus}
     end.
